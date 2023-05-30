a7ee46f1382193f98fe5f261415737324c35537c
Artem010 <cmert.123@yandex.ru>
2023-05-31 00:46:19 +0300

    add testing

--- app/home/urls.py
+++ app/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
+    path('testing', views.testing, name='testing'),
+    path('charts', views.charts, name='charts'),
 ]
